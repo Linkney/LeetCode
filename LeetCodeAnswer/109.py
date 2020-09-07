63a95eae5359bbee4b8397d30644d85e65fbbfea
Linkney <13738845806@163.com>
2020-09-07 18:49:00 +0800

    这Pig Shen老问我when

---
+++ LeetCodeAnswer/109.py
@@ -0,0 +1,53 @@
+"""
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+给定的有序链表： [-10, -3, 0, 5, 9],
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        # 二倍速指针 到达 链表末尾的时候   一倍速指针  到达中位数
+        def getMedian(left: ListNode, right: ListNode) -> ListNode:
+            fast = slow = left
+            while fast != right and fast.next != right:
+                fast = fast.next.next
+                slow = slow.next
+            return slow
+
+        # 链表递归造树
+        def buildTree(left: ListNode, right: ListNode) -> TreeNode:
+            if left == right:
+                return None
+            mid = getMedian(left, right)
+            root = TreeNode(mid.val)
+            root.left = buildTree(left, mid)
+            root.right = buildTree(mid.next, right)
+            return root
+
+        return buildTree(head, None)
