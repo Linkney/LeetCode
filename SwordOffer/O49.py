d0767e4bfe5dbaea26f521d61943b2bfd95fdafe
Linkney <13738845806@163.com>
2020-12-04 16:06:22 +0800

    明天汇报

--- SwordOffer/O49.py
+++ SwordOffer/O49.py
@@ -1,11 +1,11 @@
 """
-我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数
 
 示例:
 
 输入: n = 10
 输出: 12
-解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数
 说明:  
 
 1 是丑数。
@@ -13,6 +13,33 @@ n 不超过1690。
 """
 
 
+# 丑数的生长
+#       丑数的传递性  即  丑数 * 2 * 3 * 5 还是丑数
+#       [1]
+#       Math
+#       [1, 2, 3, 4, 5, 6, 8] 已知丑数序列 求 下一个丑数 = a*2 or b*3 or c*5
+#           且 a b c 三者各自有位序性
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        pass
+        ans = [1 for _ in range(n)]
+        # 三指针 生长
+        a, b, c = 0, 0, 0
+
+        # 生长ing
+        for i in range(1, n):
+            n2 = ans[a] * 2
+            n3 = ans[b] * 3
+            n5 = ans[c] * 5
+            ans[i] = min(n2, n3, n5)
+            if ans[i] == n2:
+                a += 1
+            if ans[i] == n3:
+                b += 1
+            if ans[i] == n5:
+                c += 1
+        return ans[-1]
+
+
+if __name__ == '__main__':
+    n = 10
+    print(Solution().nthUglyNumber(n))
---
+++ SwordOffer/O51.py
@@ -0,0 +1,19 @@
+"""
+在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+输入一个数组，求出这个数组中的逆序对的总数。
+
+示例 1:
+输入: [7,5,6,4]
+输出: 5
+"""
+
+
+class Solution:
+    # def reversePairs(self, nums: List[int]) -> int:
+    def reversePairs(self, nums):
+        pass
+
+
+if __name__ == '__main__':
+    nums = [7, 5, 6, 4]
+    print(Solution().reversePairs(nums))
